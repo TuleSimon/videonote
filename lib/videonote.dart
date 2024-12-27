c1c0d5c371e77da4c27e14517ae34f45498a8ec0
Simon-Tezda <simon@tezda.com>
2024-12-27 17:02:56 +0100

    fix

--- lib/videonote.dart
+++ lib/videonote.dart
@@ -2,5 +2,6 @@ library videonote;
 
 export 'micheals/widgets/mini_video_player.dart';
 export 'micheals/widgets/mini_video_player2.dart';
+export 'micheals/widgets/mini_video_player_better';
 export 'micheals/main.dart'; // Add any additional widgets you want to expose.
 export 'micheals/hole_widget.dart'; // Add any additional widgets you want to expose.
